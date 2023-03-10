611f9c42cfa56746a8394227cf1c65937317a8db
Phoenixxx302 <127424364+Phoenixxx302@users.noreply.github.com>
2023-03-10 09:48:26 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+let gabe = 0
+input.onGesture(Gesture.Shake, function () {
+    gabe = randint(0, 6)
+    if (gabe < 2) {
+        basic.showIcon(IconNames.Confused)
+    } else if (gabe < 4) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
 basic.forever(function () {
 	
 })
